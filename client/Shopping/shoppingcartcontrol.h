78c3e63b0a16763f31f70f5c33fe0c235850ce7e
bllanos <bernard.llanos@carleton.ca>
2014-12-06 12:19:38 -0500

    Updated ShoppingCartControl interface.

--- client/Shopping/shoppingcartcontrol.h
+++ client/Shopping/shoppingcartcontrol.h
@@ -35,14 +35,48 @@ public:
     explicit ShoppingCartControl(ViewControl *viewController = 0, QObject *parent = 0);
 
     /* Member Function: processMsg
-     *   Let the subsystem handle the message
-     * in: The message object to handle
-     * Side Effects: msg is deleted
-     * Return Value: Success indicator
+     * Handles a message received from the dispatcher.
+     * in: Message to be processed
+     * Side Effects: None
+     * Return Value: True, if the operation succeeded.
      */
-    bool processMsg(Message *msg);
+   virtual bool processMsg(const Message* msg);
 
     QWidget* getView();
+
+    // Functions that will be called upon reception of non-error information from the server
+    // -------------------------------------------------------------------------------------
+    /* All input arguments will be deallocated by these functions
+     */
+protected:
+    /* Member Function: receiveOrderConfirmation
+     *   Processes a successful order (specifically, the confirmation message)
+     * in: A confirmation message
+     * in: The receipt number of the order (also present in the confirmation message)
+     * Side Effects: None
+     * Return Value: True, if the operation succeeded
+     */
+    bool receiveOrderConfirmation(QString message, OBJ_ID_TYPE referenceNumber);
+
+    /* Note: The dispatcher will not pass error messages to subsystems,
+     * but will instead display them using the generic ViewControl::displayErrorString()
+     * function.
+     * This will be used to process unsuccessful order notifications.
+     */
+
+    // Functions to be called to send requests to the server
+    // -----------------------------------------------------
+    /* All input arguments will be deallocated by these functions
+     */
+public:
+    /* Member Function: sendOrder
+     *   Sends an order to the server for validation and processing.
+     * in: Order to be processed
+     * Side Effects: None
+     * Return Value: None
+     */
+    void processOrder(Order* order);
+
 signals:
 
 public slots:
